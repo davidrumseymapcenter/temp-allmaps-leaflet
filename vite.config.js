bbbecbf9ed87a47b227a884a49733948f1069ed7
ect123 <evanthornberry@gmail.com>
2026-04-30 10:18:11 -0700

    auto-detect all new html

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
+import { resolve } from "path";
+import { readdirSync } from "fs";
+
+const input = Object.fromEntries(
+  readdirSync(__dirname)
+    .filter((f) => f.endsWith(".html"))
+    .map((f) => [f.replace(".html", ""), resolve(__dirname, f)])
+);
+
 export default {
   base: "/temp-allmaps-leaflet/",
+  build: { rollupOptions: { input } },
 };
